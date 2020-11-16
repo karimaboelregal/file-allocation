845b9f206e9fe4b8b7b609eb536be0d3aec3bc68
jjohnnyboi <74542306+jjohnnyboi@users.noreply.github.com>
2020-11-16 09:18:59 -0800

    Block class and main class
    main class to do

---
+++ FileAllocation.java
@@ -0,0 +1,8 @@
+package file.allocation;
+import java.util.*;
+
+public class FileAllocation {
+    public static void main(String[] args) {
+    	// to do
+    }
+}
---
+++ block.java
@@ -0,0 +1,7 @@
+package file.allocation;
+
+public class block {
+    String fileName;
+    int blockNo;
+    boolean occupied = false;
+}
