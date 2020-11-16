8220cfc68d9597deb088a9b6bb430b8f67d95278
jjohnnyboi <74542306+jjohnnyboi@users.noreply.github.com>
2020-11-16 09:24:54 -0800

    updated main
    made the first do extended loop and added init function

--- FileAllocation.java
+++ FileAllocation.java
@@ -2,7 +2,28 @@ package file.allocation;
 import java.util.*;
 
 public class FileAllocation {
+    static block[] blockMem;
+
+    static void init(int length) {
+        blockMem = new block[length];
+        for (int i = 0; i < length; i++) {
+            blockMem[i] = new block();
+        }
+    }
+
     public static void main(String[] args) {
-    	// to do
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the block size");
+        int size = input.nextInt();
+        boolean cont = false;
+        String fileName;
+        int start, length;
+        init(size);
+        do {
+            System.out.println("Enter the file name and length");
+            fileName = input.next();
+            length = input.nextInt();
+            start = getRandomNumber(0, (size - 1) - length);
+        } while(cont == true);
     }
 }
